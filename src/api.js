ff650302f3a073169117788cb6030c084e0811f0
Aditya B. <96489237+adityaaditya98@users.noreply.github.com>
2025-08-25 00:10:41 +0530

    finalOneDeply

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://smtp-and-imap-backend-git-8ce6a0-badityaaditya98-9228s-projects.vercel.app",
+    baseURL: process.env.baseURL,
 })
 
 const sendEmail = async (data) => {
